dd2818da0a0a489ab0cd4c0772afaf9fb436439d
Tom Karim <tomkarim6@gmail.com>
2018-06-10 20:56:37 -0400

    added actors and display in idea, couldn't figure out a way to use processing

---
+++ PacMan/src/Display.java
@@ -0,0 +1,81 @@
+import java.awt.*;
+import java.util.*;
+import java.lang.*;
+
+public class Display extends Canvas{
+    private Game game;
+    private boolean running;
+    private BufferStrategy bs;
+
+    public Display(Game game) {
+        this.game = game;
+        int sx = (int) (game.screenSize.width * game.screenScale.getX());
+        int sy = (int) (game.screenSize.height * game.screenScale.getY());
+        setPreferredSize(new Dimension(sx, sy));
+        addKeyListener(new Keyboard());
+    }
+
+    public void start() {
+        if (running) {
+            return;
+        }
+        createBufferStrategy(3);
+        bs = getBufferStrategy();
+        game.initialize();
+        running = true;
+        Thread thread = new Thread(new MainLoop());
+        thread.start();
+    }
+
+    private class MainLoop implements Runnable {
+
+        @Override
+        public void run() {
+            long desiredFrameRateTime = 1000 / 60;
+            long currentTime = System.currentTimeMillis();
+            long lastTime = currentTime - desiredFrameRateTime;
+            long unprocessedTime = 0;
+            boolean needsRender = false;
+            while (running) {
+                currentTime = System.currentTimeMillis();
+                unprocessedTime += currentTime - lastTime;
+                lastTime = currentTime;
+
+                while (unprocessedTime >= desiredFrameRateTime) {
+                    unprocessedTime -= desiredFrameRateTime;
+                    update();
+                    needsRender = true;
+                }
+
+                if (needsRender) {
+                    Graphics2D graph = (Graphics2D) bs.getDrawGraphics();
+                    graph.setBackground(Color.BLACK);
+                    graph.clearRect(0, 0, getWidth(), getHeight());
+                    graph.scale(game.screenScale.getX(), game.screenScale.getY());
+                    draw(graph);
+                    graph.dispose();
+                    bs.show();
+                    needsRender = false;
+                }
+                else {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException ex) {
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void update() {
+        game.update();
+    }
+
+    public void draw(Graphics2D graph) {
+        game.draw(graph);
+    }
+
+}
+
+}
---
+++ PacMan/src/Game.java
@@ -0,0 +1,44 @@
+
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    public Dimension size;
+    public Point2D scale;
+
+    public ArrayList<Actor> actors = new ArrayList<>();
+
+    public void initialize() {
+    }
+
+    public void update() {
+        for (Actor actor : actors) {
+            actor.update();
+        }
+    }
+
+    public void draw(Graphics2D graph) {
+        for (Actor actor : actors) {
+            actor.draw(graph);
+        }
+    }
+
+    public <Typ> Typ collisionChecker(Actor a1, Class<Typ> type) {
+        a1.updateCollider();
+        for(Actor a2 : actors) {
+            a2.updateCollider();
+            if (a1 != a2
+                    && type.isInstance(a2)
+                    && a1.collider != null && a2.collider != null
+                    && a1.visible && a2.visible
+                    && a2.collider.intersects(a1.collider)) {
+                return type.cast(a2);
+            }
+        }
+        return null;
+    }
+}
